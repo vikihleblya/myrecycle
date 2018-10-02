3003b8ff84a9d9f53ff689fd3b5520a384fc352f
vikihleblya <vikihleblya@gmail.com>
2018-10-02 17:53:35 +0300

    Get to next activity via toolbar

--- app/src/main/java/com/example/grigviktor/myrecycle/GetRequestStepOne.kt
+++ app/src/main/java/com/example/grigviktor/myrecycle/GetRequestStepOne.kt
@@ -3,8 +3,10 @@ package com.example.grigviktor.myrecycle
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.support.v7.view.menu.MenuBuilder
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_get_request_step_one.*
 
 
 class GetRequestStepOne : AppCompatActivity() {
@@ -12,7 +14,27 @@ class GetRequestStepOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_request_step_one)
+        setSupportActionBar(toolbar)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menu?.let {
+            if (menu is MenuBuilder) {
+                try {
+                    val field = menu.javaClass.getDeclaredField("mOptionalIconsVisible")
+                    field.isAccessible = true
+                    field.setBoolean(menu, true)
+                } catch (ignored: Exception) {
+                    // ignored exception
+                }
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
 }
 
 
